917e241aa4c395510c1e66f3fab51a5cc36aa73a
veereshbasanna <veer.basanna@gmail.com>
2025-05-06 16:05:25 +0530

    first commit

---
+++ test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "How are you"
+
+s="bash script"
+
+echo "Hello goodmoning"
+echo "wele come to $s"
+echo "hello $s"
+echo " bash $s good"
+
+
+
+echo -e "fourthline\nfifthline\nsixtline"
+
+echo -e "fourthline\tfifthline\tsixthlne"
+
---
+++ test5.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+echo "How are you" 
+echo" welcome to the courses"
+
+<< mycom
+for a in 1 2 3 4 5
+do 
+	echo "welcome $a "
+
+done
+mycom
+
+
+for a in {1..5}
+
+do
+      touch samp.$a
+done
+
+
